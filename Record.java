b486ca0c69503de3fac6e07e15efb6944c23bc59
Hudson, Philip <Philip.Hudson@zuhlke.com>
2017-09-13 18:05:13 +0100

    initial commit

--- Record.java
+++ Record.java
@@ -1,5 +1,6 @@
 package com.philhudson.topicteams;
 
+import java.util.ArrayList;
 import java.util.Set;
 
 public class Record {
@@ -14,7 +15,7 @@ public class Record {
         this.project = project;
     }
 
-    public boolean isAllowedOnTable(Set<Record> records) {
+    public boolean isAllowedOnTable(Set<Record> records, int rotation, int currentTableNo, ArrayList<ArrayList<Set<Record>>> tableRotation) {
         boolean isAllowed = true;
 
         if(records.size() > 8) {
@@ -29,6 +30,34 @@ public class Record {
             }
         }
 
-        return noOnSameProject <= 2;
+        if( noOnSameProject > 2) {
+            return false;
+        }
+
+
+        if(rotation >= 1) {
+            for(int x = 0; x < rotation; x++ ) {
+
+                ArrayList<Set<Record>> lastTableSetup = tableRotation.get(x);
+
+                // which table did I sit at last time?
+                int lastTableISatAt =-1;
+
+                for (int i = 0; i < 7; i++) {
+                    for (Record r: lastTableSetup.get(i)) {
+                        if(r.surname.equals(this.surname) && r.firstname.equals(this.firstname)) {
+                            lastTableISatAt = i;
+                        }
+                    }
+                }
+
+                if(currentTableNo == lastTableISatAt) {
+                    return false;
+                }
+            }
+
+        }
+
+        return true;
     }
 }
--- TopicTeams.java
+++ TopicTeams.java
@@ -17,12 +17,10 @@ public class TopicTeams {
 
         Set<Record> names = new HashSet<>();
         Set<String> projects = new HashSet<>();
-        ArrayList<Set<Record>> tables = new ArrayList<>();
-        ArrayList<Record> topicTeamLead = new ArrayList<>();
 
-        for (int i = 0; i <= 8; i++) {
-            tables.add(new HashSet<>());
-        }
+        ArrayList<Record> topicTeamLead = new ArrayList<>();
+        ArrayList<ArrayList<Set<Record>>> tableRotation = new ArrayList<>();
+
 
         try (Stream<String> stream = Files.lines(Paths.get(fileName))) {
 
@@ -38,43 +36,52 @@ public class TopicTeams {
                 }
             });
 
-            ArrayList<Record> cloneNames = new ArrayList<>();
-            cloneNames.addAll(names);
+            for(int rotation = 0; rotation < 8; rotation++ ) {
 
-            int startingTableNo = 0;
+                ArrayList<Set<Record>> tables = new ArrayList<>();
 
-            while (cloneNames.size() > 0) {
-                Record n = cloneNames.remove(ThreadLocalRandom.current().nextInt(0, cloneNames.size()));
-
-                int tableNo = startingTableNo;
-                boolean allocated = false;
-                do {
-                    if (n.isAllowedOnTable(tables.get(tableNo))) {
-                        tables.get(tableNo).add(n);
-                        allocated = true;
-                    } else {
-                        tableNo++;
-                        if(tableNo >8) tableNo = 0;
-                    }
-                } while (tableNo != startingTableNo && !allocated);
-
-                startingTableNo++;
-                if (startingTableNo > 8 ) startingTableNo =0;
-            }
-
-            System.out.println("--------------- Tables");
-            for (int i = 0; i < 8; i++) {
-                Record ttl = topicTeamLead.get(i);
-                System.out.println(i + "..." + ttl.firstname + " " + ttl.surname);
-                for (Record name : tables.get(i)) {
-                    System.out.println("\t"  + name.firstname + " " + name.surname + " (" + name.project + ")");
+                for (int i = 0; i <= 8; i++) {
+                    tables.add(new HashSet<>());
                 }
+
+                ArrayList<Record> cloneNames = new ArrayList<>();
+                cloneNames.addAll(names);
+
+                int startingTableNo = 0;
+
+                while (cloneNames.size() > 0) {
+                    Record n = cloneNames.remove(ThreadLocalRandom.current().nextInt(0, cloneNames.size()));
+
+                    int tableNo = startingTableNo;
+                    boolean allocated = false;
+                    do {
+                        if (n.isAllowedOnTable(tables.get(tableNo), rotation, tableNo, tableRotation)) {
+                            tables.get(tableNo).add(n);
+                            allocated = true;
+                        } else {
+                            tableNo++;
+                            if(tableNo >8) tableNo = 0;
+                        }
+                    } while (tableNo != startingTableNo && !allocated);
+
+                    startingTableNo++;
+                    if (startingTableNo > 8 ) startingTableNo = 0;
+                }
+
+                System.out.println("--------------- Tables");
+                for (int i = 0; i < 8; i++) {
+                    Record ttl = topicTeamLead.get(i);
+                    System.out.println(i + "..." + ttl.firstname + " " + ttl.surname);
+                    for (Record name : tables.get(i)) {
+                        System.out.println("\t"  + name.firstname + " " + name.surname + " (" + name.project + ")");
+                    }
+                }
+
+                tableRotation.add(tables);
             }
 
         } catch (IOException e) {
             e.printStackTrace();
         }
-
-
     }
 }
